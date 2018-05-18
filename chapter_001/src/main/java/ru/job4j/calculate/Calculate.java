0f0df40c1a3c444c198a8e34bf8f78b10e2430e2
HipNoR <hipnorosva@gmail.com>
2018-05-18 19:51:47 +0300

    1.3. JavDoc. Code convetion.

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,10 +1,20 @@
 package ru.job4j.calculate;
 
+/**
+*Class calculate решение задачи части 001 урок 1.
+*@author Roman Bednyashov (hipnorosva@gmail.com)
+*@since 0.1
+*@version 0.1
+*/
+
 public class Calculate {
+	/**
+	*Метод main
+	*@param args - args
+	*/
 	
 	public static void main(String[] args) {
 		System.out.println("Hello World");
     }
-	
-		
+			
 }
