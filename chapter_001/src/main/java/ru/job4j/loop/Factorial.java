466978ac4a60de58002da466ac2eeecf71e2855e
HipNoR <hipnorosva@gmail.com>
2018-05-22 12:57:44 +0300

    4.2. Создать программу вычисляющую факториал.

---
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -0,0 +1,27 @@
+package ru.job4j.loop;
+
+/**
+ * Class Factorial.
+ * @author Roman Bednyashov (hipnorosva@gmail.com).
+ * @since 0.1
+ * @version 0.1
+ */
+public class Factorial {
+
+    /**
+     * Method calculates the factorial of a number.
+     * @param n input the number.
+     * @return factorial of the number n.
+     */
+    public int calc(int n) {
+        int sum = 1;
+        if (n == 0) {
+            sum = 1;
+        } else {
+            for (int i = 1; i <= n; i++) {
+                sum *= i;
+            }
+        }
+        return sum;
+    }
+}
