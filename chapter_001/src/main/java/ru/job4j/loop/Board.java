dda73c3485d8c9b08658be2422c694539520339f
HipNoR <hipnorosva@gmail.com>
2018-05-22 16:18:52 +0300

    4.4. Построить пирамиду в псевдографике.

--- chapter_001/src/main/java/ru/job4j/loop/Board.java
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -1,12 +1,19 @@
 package ru.job4j.loop;
 
 /**
- * Class Board - board for chess.
+ * Class Board - chessboard.
  * @author Roman Bednyashov (hipnorosva@gmail.com).
  * @since 0.1
  * @version 0.1
  */
 public class Board {
+
+    /**
+     * Method draws a chessboard.
+     * @param width - width of the board.
+     * @param height - height of the board.
+     * @return - returns the chessboard to the terminal.
+     */
     public String paint(int width, int height) {
         StringBuilder screen = new StringBuilder();
         String ln = System.lineSeparator();
