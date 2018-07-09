9f67bb17ace9dd3c3d8dda26e37260591f36f05a
HipNoR <hipnorosva@gmail.com>
2018-07-09 13:36:07 +0300

    5.2.1. Реализовать SimpleArray<T> [#156] v1

---
+++ chapter_005/src/main/java/ru/job4j/services/SimpleArray.java
@@ -0,0 +1,149 @@
+package ru.job4j.services;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Class wrapper for the array.
+ * The array has a fixed size.
+ * @param <T> generic.
+ */
+public class SimpleArray<T> implements Iterable<T> {
+    private Object[] objects;
+    private int position = 0;
+    private int size = 0;
+
+    public SimpleArray(int size) {
+        this.objects = new Object[size];
+        this.size = size;
+    }
+
+    /**
+     * Method add element to array.
+     * If array is full - throws exception.
+     * @param model object to be added.
+     */
+    public void add(T model) {
+        outOfArray(this.position);
+        this.objects[position++] = model;
+    }
+
+    /**
+     * Replace element in array by another.
+     * Throws an exception if the index is greater than the size.
+     * @param index element to be replaced.
+     * @param model the element that replaces.
+     */
+    public void set(int index, T model) {
+        outOfArray(index);
+        this.objects[index] = model;
+    }
+
+    /**
+     * Gets the element from the array.
+     * Throws an exception if the index is greater than the size.
+     * @param index position of the element.
+     * @return element in the index position.
+     */
+    public T get(int index) throws NoSuchElementException {
+        outOfArray(index);
+        if (this.objects[index] == null) {
+            throw new NoSuchElementException();
+        }
+        return (T) this.objects[index];
+    }
+
+    /**
+     * Removes an element at index.
+     * Throws an exception if the index is greater than the size.
+     * @param index of element to be deleted.
+     */
+    public void delete(int index) throws NoSuchElementException {
+        outOfArray(index);
+        if (this.objects[index] == null) {
+            throw new NoSuchElementException();
+        }
+        if (index == size - 1) {
+            objects[index] = null;
+        } else {
+            System.arraycopy(objects, index + 1, objects, index, size - index - 1);
+            objects[size - 1] = null;
+        }
+    }
+
+    /**
+     * Checks the index outside the array.
+     * @param index to check.
+     * @throws IndexOutOfBoundsException if index greater than arrays size.
+     */
+    private void outOfArray(int index) throws IndexOutOfBoundsException {
+        if (index >= this.size) {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+
+    /**
+     * Returns an iterator over the elements in this list in proper sequence.
+     * @return an iterator.
+     */
+    @Override
+    public Iterator<T> iterator() {
+        return new Itr();
+    }
+
+    private class Itr implements Iterator<T> {
+        /**
+         * Index of element to be returned by subsequent call to next.
+         */
+        int cursor = 0;
+
+        /**
+         * Index of element returned by most recent call to next or
+         * previous.  Reset to -1 if this element is deleted by a call
+         * to remove.
+         */
+        int lastRet = -1;
+
+        @Override
+        public boolean hasNext() {
+            boolean valid = false;
+            if (cursor != size && objects[cursor] != null) {
+                valid = true;
+            }
+            return valid;
+        }
+
+        @Override
+        public T next() {
+            try {
+                if (!hasNext()) {
+                    throw new IndexOutOfBoundsException();
+                }
+                int i = cursor;
+                T next = get(i);
+                lastRet = i;
+                cursor = i + 1;
+                return next;
+            } catch (IndexOutOfBoundsException e) {
+                throw new NoSuchElementException();
+            }
+        }
+
+        @Override
+        public void remove() {
+            if (lastRet < 0) {
+                throw new IllegalStateException();
+            }
+            try {
+                delete(lastRet);
+                if (lastRet < cursor) {
+                    cursor--;
+                }
+                lastRet = -1;
+            } catch (IndexOutOfBoundsException e) {
+                throw new NoSuchElementException();
+            }
+        }
+    }
+}
---
+++ chapter_005/src/test/java/ru/job4j/services/SimpleArrayTest.java
@@ -0,0 +1,101 @@
+package ru.job4j.services;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class SimpleArrayTest {
+    SimpleArray<Integer> result;
+
+    @Before
+    public void create() {
+        result = new SimpleArray<>(5);
+        result.add(1);
+        result.add(2);
+        result.add(3);
+    }
+
+    @Test
+    public void whenAddThenAdded() {
+        result.add(8);
+        assertThat(result.get(3), is(8));
+    }
+
+    @Test (expected = IndexOutOfBoundsException.class)
+    public void whenAddMoreThanSizeThenException() {
+        result.add(4);
+        result.add(5);
+        result.add(6);
+    }
+
+    @Test
+    public void whenSetThenSets() {
+        result.set(1, 10);
+        assertThat(result.get(1), is(10));
+    }
+
+    @Test (expected = IndexOutOfBoundsException.class)
+    public void whenSetOutOfBoundsThenException() {
+        result.set(5, 10);
+    }
+
+    @Test (expected = IndexOutOfBoundsException.class)
+    public void whenGetOutOfBoundsThenException() {
+        result.get(5);
+    }
+
+    @Test (expected = NoSuchElementException.class)
+    public void whenGetNullThanException() {
+        result.get(4);
+    }
+
+    @Test
+    public void whenDeleteExistingThenDelete() {
+        result.delete(1);
+        assertThat(result.get(1), is(3));
+    }
+
+    @Test (expected = IndexOutOfBoundsException.class)
+    public void whenDeleteOutOfBoundsThenException() {
+        result.delete(5);
+    }
+
+    @Test
+    public void whenUseIteratorThen() {
+        Iterator<Integer> itr = result.iterator();
+        assertThat(itr.hasNext(), is(true));
+        assertThat(itr.next(), is(1));
+        assertThat(itr.hasNext(), is(true));
+        assertThat(itr.next(), is(2));
+        assertThat(itr.hasNext(), is(true));
+        assertThat(itr.next(), is(3));
+        assertThat(itr.hasNext(), is(false));
+    }
+
+    @Test (expected = NoSuchElementException.class)
+    public void whenRemoveThenRemoved() {
+        Iterator<Integer> itr = result.iterator();
+        result.add(4);
+        result.add(5);
+        itr.next();
+        itr.remove();
+        assertThat(result.get(0), is(2));
+        result.get(4);
+    }
+
+
+    @Test (expected = NoSuchElementException.class)
+    public void whenUseNextOutOFBoundsThenException() {
+        Iterator<Integer> itr = result.iterator();
+        itr.next();
+        itr.next();
+        itr.next();
+        itr.next();
+    }
+
+}
