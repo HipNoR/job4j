1a65e2339d7c78ba0ac4c881038b4ce8a0c4731e
HipNoR <hipnorosva@gmail.com>
2018-07-11 12:41:48 +0300

    5.3.1. Создать динамический список на базе массива.[#60826]

--- chapter_005/src/main/java/ru/job4j/list/SimpleArrayList.java
+++ chapter_005/src/main/java/ru/job4j/list/SimpleArrayList.java
@@ -1,71 +1,210 @@
 package ru.job4j.list;
 
+import java.util.Arrays;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
 /**
- * Simple linked list class.
+ * Array list class.
+ * Dynamically expandable storage container for objects.
  * @author Roman Bednyashov (hipnorosva@gmail.com)
  * @version 0.1$
  * @since 0.1
  * 11.07.2018
  */
-public class SimpleArrayList<E> {
-
-    private int size;
-    private Node<E> first;
+public class SimpleArrayList<T> {
+    /**
+     * Container for objects.
+     */
+    private Object[] container;
 
     /**
-     * Method adds an element to the head of the list.
-     * @param date
+     * Index on which item will be added.
      */
-    public void add(E date) {
-        Node<E> newLink = new Node<>(date);
-        newLink.next = this.first;
-        this.first = newLink;
-        this.size++;
+    private int position = 0;
+
+    /**
+     * The size of the container.
+     */
+    private int size = 0;
+
+    /**
+     * The count of modifications for the iterator working.
+     */
+    private int modCount = 0;
+
+    public SimpleArrayList() {
+        this.container = new Object[10];
+        this.size = 10;
     }
 
-    /**
-     * Method deletes an element at the head of the list.
-     * @return deleted element.
-     */
-    public E delete() {
-        E result = first.date;
-        first = first.next;
-        this.size--;
-        return result;
+    public SimpleArrayList(int capacity) {
+        this.container = new Object[capacity];
+        this.size = capacity;
     }
 
-    /**
-     * Method gets the element by index.
-     * @param index to get.
-     * @return element.
-     */
-    public E get(int index) {
-        Node<E> result = this.first;
-        for (int i = 0; i < index; i++) {
-            result = result.next;
-        }
-        return result.date;
-    }
-
-    /**
-     * Method gets the size of the collections.
-     * @return size of collection.
-     */
-    public int getSize() {
+    public int size() {
         return this.size;
     }
 
     /**
-     * Class for data storage.
-     * @param <E> type.
+     * Method add element to array.
+     * If array is full - throws exception.
+     * @param model object to be added.
      */
-    private static class Node<E> {
+    public void add(T model) {
+        ensureCapacity();
+        this.container[position++] = model;
+    }
 
-        E date;
-        Node<E> next;
+    /**
+     * Method for ensuring the container size.
+     * When you add the last element to the current size, it doubles it.
+     */
+    private void ensureCapacity() {
+        if (size == position) {
+            int newCapacity = size * 2;
+           // Object[] newArray = Arrays.copyOf(container, newCapacity);
+            container = Arrays.copyOf(container, newCapacity);
+            this.size = newCapacity;
+            modCount++;
+        }
+    }
 
-        Node(E date) {
-            this.date = date;
+    /**
+     * Replace element in array by another.
+     * Throws an exception if the index is greater than the size.
+     * @param index element to be replaced.
+     * @param model the element that replaces.
+     */
+    public void set(int index, T model) {
+        outOfArray(index);
+        this.container[index] = model;
+        modCount++;
+    }
+
+    /**
+     * Gets the element from the array.
+     * Throws an exception if the index is greater than the size.
+     * @param index position of the element.
+     * @return element in the index position.
+     */
+    public T get(int index) throws NoSuchElementException {
+        outOfArray(index);
+        if (this.container[index] == null) {
+            throw new NoSuchElementException();
+        }
+        return (T) this.container[index];
+    }
+
+    /**
+     * Removes an element at index.
+     * Throws an exception if the index is greater than the size.
+     * @param index of element to be deleted.
+     */
+    public void delete(int index) throws NoSuchElementException {
+        outOfArray(index);
+        if (index == size - 1) {
+            container[index] = null;
+            modCount++;
+        } else {
+            System.arraycopy(container, index + 1, container, index, size - index - 1);
+            container[size - 1] = null;
+            modCount++;
+        }
+
+    }
+
+    /**
+     * Checks the index outside the array.
+     * @param index to check.
+     * @throws IndexOutOfBoundsException if index greater than arrays size.
+     */
+    private void outOfArray(int index) throws IndexOutOfBoundsException {
+        if (index >= this.size) {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+
+    /**
+     * Returns an iterator over the elements in this list in proper sequence.
+     * @return an iterator.
+     */
+    public Iterator<T> iterator() {
+        return new SimpleArrayList.Itr();
+    }
+
+    private class Itr implements Iterator<T> {
+        /**
+         * Index of element to be returned by subsequent call to next.
+         */
+        int cursor = 0;
+
+        /**
+         * Index of element returned by most recent call to next or
+         * previous.  Reset to -1 if this element is deleted by a call
+         * to remove.
+         */
+        int lastRet = -1;
+
+        /**
+         *
+         */
+        int expectedModCount = modCount;
+
+        @Override
+        public boolean hasNext() {
+            checkForModifications();
+            boolean valid = false;
+            if (cursor != size && container[cursor] != null) {
+                valid = true;
+            }
+            return valid;
+        }
+
+        @Override
+        public T next() {
+            checkForModifications();
+            try {
+                if (!hasNext()) {
+                    throw new IndexOutOfBoundsException();
+                }
+                int i = cursor;
+                T next = get(i);
+                lastRet = i;
+                cursor = i + 1;
+                return next;
+            } catch (IndexOutOfBoundsException e) {
+                throw new NoSuchElementException();
+            }
+        }
+
+        @Override
+        public void remove() {
+            checkForModifications();
+            if (lastRet < 0) {
+                throw new IllegalStateException();
+            }
+            try {
+                delete(lastRet);
+                    cursor = lastRet;
+                expectedModCount = modCount;
+                lastRet = -1;
+            } catch (IndexOutOfBoundsException e) {
+                throw new NoSuchElementException();
+            }
+        }
+
+        /**
+         * Checks the container for changes after Iterator initialization.
+         * @throws ConcurrentModificationException if modified.
+         */
+        private void checkForModifications() throws ConcurrentModificationException {
+            if (expectedModCount != modCount) {
+                throw new ConcurrentModificationException();
+            }
         }
     }
 }
--- chapter_005/src/test/java/ru/job4j/list/SimpleArrayListTest.java
+++ chapter_005/src/test/java/ru/job4j/list/SimpleArrayListTest.java
@@ -1,35 +1,125 @@
 package ru.job4j.list;
 
-import org.junit.Test;
 import org.junit.Before;
-import static org.hamcrest.Matchers.is;
+import org.junit.Test;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class SimpleArrayListTest {
-
-    private SimpleArrayList<Integer> list;
+    SimpleArrayList<Integer> result;
 
     @Before
-    public void beforeTest() {
-        list = new SimpleArrayList<>();
-        list.add(1);
-        list.add(2);
-        list.add(3);
+    public void create() {
+        result = new SimpleArrayList<>();
+        result.add(1);
+        result.add(2);
+        result.add(3);
     }
 
     @Test
-    public void whenAddThreeElementsThenUseGetOneResultTwo() {
-        assertThat(list.get(1), is(2));
+    public void whenAddThenAdded() {
+        result.add(8);
+        assertThat(result.get(3), is(8));
     }
 
     @Test
-    public void whenAddThreeElementsThenUseGetSizeResultThree() {
-        assertThat(list.getSize(), is(3));
+    public void whenSetThenSets() {
+        result.set(1, 10);
+        assertThat(result.get(1), is(10));
+    }
+
+    @Test (expected = IndexOutOfBoundsException.class)
+    public void whenSetOutOfBoundsThenException() {
+        result.set(11, 10);
+    }
+
+    @Test (expected = IndexOutOfBoundsException.class)
+    public void whenGetOutOfBoundsThenException() {
+        result.get(11);
+    }
+
+    @Test (expected = NoSuchElementException.class)
+    public void whenGetNullThanException() {
+        result.get(4);
     }
 
     @Test
-    public void whenDeleteFirstThenUseGetSizeResultTwo() {
-        assertThat(list.delete(), is(3));
-        assertThat(list.getSize(), is(2));
+    public void whenDeleteExistingThenDelete() {
+        result.delete(1);
+        assertThat(result.get(1), is(3));
+    }
+
+    @Test (expected = IndexOutOfBoundsException.class)
+    public void whenDeleteOutOfBoundsThenException() {
+        result.delete(11);
+    }
+
+    @Test
+    public void whenUseIteratorThen() {
+        Iterator<Integer> itr = result.iterator();
+        assertThat(itr.hasNext(), is(true));
+        assertThat(itr.next(), is(1));
+        assertThat(itr.hasNext(), is(true));
+        assertThat(itr.next(), is(2));
+        assertThat(itr.hasNext(), is(true));
+        assertThat(itr.next(), is(3));
+        assertThat(itr.hasNext(), is(false));
+    }
+
+    @Test (expected = NoSuchElementException.class)
+    public void whenRemoveThenRemoved() {
+        Iterator<Integer> itr = result.iterator();
+        result.add(4);
+        result.add(5);
+        itr.next();
+        itr.remove();
+        assertThat(result.get(1), is(3));
+        result.get(4);
+    }
+
+
+    @Test (expected = NoSuchElementException.class)
+    public void whenUseNextOutOFBoundsThenException() {
+        Iterator<Integer> itr = result.iterator();
+        itr.next();
+        itr.next();
+        itr.next();
+        itr.next();
+    }
+
+    @Test (expected = ConcurrentModificationException.class)
+    public void whenModifiedWhenIterateThenException() {
+        Iterator<Integer> itr = result.iterator();
+        itr.next();
+        result.delete(0);
+        itr.next();
+
+    }
+
+    @Test
+    public void whenEnsureCapacityThenSizeDoubled() {
+        result = new SimpleArrayList<>(2);
+        result.add(1);
+        result.add(2);
+        result.add(3);
+        assertThat(result.size(), is(4));
+    }
+
+    @Test (expected = ConcurrentModificationException.class)
+    public void whenEnsureCapacityWhenIterateThenException() {
+        result = new SimpleArrayList<>(2);
+        result.add(1);
+        result.add(2);
+        Iterator<Integer> itr = result.iterator();
+        assertThat(itr.next(), is(1));
+        assertThat(itr.next(), is(2));
+        result.add(3);
+        itr.next();
+
     }
 }
