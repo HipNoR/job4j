24f4f9ecd4d1e16c177446ee7c19d83f23861831
HipNoR <hipnorosva@gmail.com>
2018-07-16 08:31:20 +0300

    5.3.4. Задан связанный список. Определить цикличность. [#161] v0.2

--- chapter_005/src/main/java/ru/job4j/list/Node.java
+++ chapter_005/src/main/java/ru/job4j/list/Node.java
@@ -3,7 +3,7 @@ package ru.job4j.list;
 /**
  * Simple node class for linked list.
  * @author Roman Bednyashov (hipnorosva@gmail.com)
- * @version 0.1$
+ * @version 0.2$
  * @since 0.1
  * 12.07.2018
  */
@@ -17,17 +17,18 @@ public class Node<T> {
 
     /**
      * The method determines whether there is a circular reference in the container.
+     * Used Floyd's Tortoise and Hare algorithm.
      * @param node first element to start.
      * @return true if cycle else false.
      */
     boolean hasCycle(Node<T> node) {
         boolean valid = false;
-        String loop = "";
-        Node<T> temp = node;
-        while (temp.next != null) {
-            loop += temp.toString();
-            temp = temp.next;
-            if (loop.contains(temp.toString())) {
+        Node<T> slow = node;
+        Node<T> fast = node;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
                 valid = true;
                 break;
             }
