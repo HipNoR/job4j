8f9bcb0ea6d46564161b53b2b2438624dde79054
HipNoR <hipnorosva@gmail.com>
2018-10-12 22:32:04 +0300

    Фильтры [#1732] v 0.2

--- chapter_007/src/main/SQL/products.sql
+++ chapter_007/src/main/SQL/products.sql
@@ -1,7 +1,9 @@
 
 --1. Написать запрос получение всех продуктов с типом "СЫР"
-SELECT * FROM product
-WHERE type_id = (SELECT id FROM type WHERE name = 'СЫР');
+SELECT * FROM product AS p
+  INNER JOIN type AS t
+    ON p.type_id = t.id
+WHERE t.name = 'СЫР';
 
 --2. Написать запрос получения всех продуктов, у кого в имени есть слово "мороженное"
 SELECT * FROM product
@@ -19,27 +21,29 @@ LIMIT 1;
 
 --5. Написать запрос, который выводит количество всех продуктов определенного типа.
 SELECT t.name, count(type_id) FROM product as p
-INNER JOIN type as t
-ON p.type_id = t.id
+  INNER JOIN type as t
+    ON p.type_id = t.id
 GROUP BY t.name;
 
 --6. Написать запрос получение всех продуктов с типом "СЫР" и "МОЛОКО"
-SELECT * FROM product
-WHERE 
-type_id = (SELECT id FROM type WHERE name = 'СЫР') 
-OR
-type_id = (SELECT id FROM type WHERE name = 'МОЛОКО');
+SELECT * FROM product AS p
+  INNER JOIN type AS t
+    ON p.type_id = t.id
+WHERE
+  t.name = 'СЫР'
+  OR
+  t.name = 'МОЛОКО';
 
 --7. Написать запрос, который выводит тип продуктов, которых осталось меньше 10 штук. 
 SELECT t.name, count(type_id) AS value FROM product as p
-INNER JOIN type as t
-ON p.type_id = t.id
+  INNER JOIN type as t
+    ON p.type_id = t.id
 GROUP BY t.name
 HAVING count(type_id) < 10;
 
 --8. Вывести все продукты и их тип.
 SELECT p.name, t.name FROM
-product AS p
-INNER JOIN 
-type AS t
-ON p.type_id = t.id;
+  product AS p
+  INNER JOIN
+  type AS t
+    ON p.type_id = t.id;
