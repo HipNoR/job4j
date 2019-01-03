256e9d4bae701fe4eb0860f6d7df0aa8e94ed182
HipNoR <hipnorosva@gmail.com>
2019-01-03 14:54:25 +0300

    2. Удаление запрещенных слов [#859] v0.2

--- chapter_009/src/main/java/ru/job4j/abusechecker/AbuseFilter.java
+++ chapter_009/src/main/java/ru/job4j/abusechecker/AbuseFilter.java
@@ -4,15 +4,13 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Class stream filter character.
  *
  * @author Roman Bednyashov (hipnorosva@gmail.com)
- * @version 0.1$
+ * @version 0.2$
  * @since 0.1
  * 29.12.2018
  */
@@ -28,33 +26,47 @@ public class AbuseFilter {
      * @param out output stream.
      * @param abuse array of filterable words.
      */
+
     public void dropAbuses(InputStream in, OutputStream out, String[] abuse) {
-        var abused = List.of(abuse);
-        boolean available = true;
-        StringBuilder sb = new StringBuilder();
-        try (var input = in; var bis = new BufferedInputStream(input)) {
-            char c;
-            while (available) {
-                if (bis.available() == 0)  {
-                    available = false;
-                }
-                c = (char) bis.read();
-                if (c == ' ' || !available) {
-                    var word = sb.toString();
-                    if (!abused.contains(word)) {
-                        LOG.info("Word \"{}\" passed the filter and passed to the output stream.", word);
-                        word += " ";
-                        out.write(word.getBytes());
-                    } else {
-                        LOG.info("Word \"{}\" has not passed the filter and deleted.", word);
+        new BufferedReader(new InputStreamReader(in)).lines()
+                .map(line -> {
+                    for (String word : abuse) {
+                        line = line.replaceAll(word, "");
                     }
-                    sb = new StringBuilder();
-                } else {
-                    sb.append(c);
-                }
+                    return line.replaceAll(" {2,}", " ").strip() + "\n";
+                }).forEach(line -> {
+            try {
+                out.write(line.getBytes());
+            } catch (IOException e) {
+                LOG.error("IOException", e);
             }
-        } catch (IOException e) {
-            LOG.error("Error", e);
+        });
+    }
+
+    public void dropAbusesSecond(InputStream in, OutputStream out, String[] abuse) throws IOException {
+        out.write(
+                new BufferedReader(new InputStreamReader(in))
+                        .lines()
+                        .map(line -> {
+                            for (String word : abuse) {
+                                line = line.replaceAll(word, "");
+                            }
+                            return line.replaceAll(" {2,}", " ").strip();
+                        }).collect(Collectors.joining("\n")).getBytes()
+        );
+    }
+
+   public void dropAbuseUniversal(InputStream in, OutputStream out, String[] abuse) throws IOException {
+        try (var reader = new BufferedReader(new InputStreamReader(in));
+             var writer = new BufferedWriter(new OutputStreamWriter(out))
+        ) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                for (var word : abuse) {
+                    line = line.replaceAll(word, "");
+                }
+                writer.write(line.replaceAll(" {2,}", " ").strip() + "\n");
+            }
         }
     }
 }
--- chapter_009/src/test/java/ru/job4j/abusechecker/AbuseFilterTest.java
+++ chapter_009/src/test/java/ru/job4j/abusechecker/AbuseFilterTest.java
@@ -3,22 +3,89 @@ package ru.job4j.abusechecker;
 import org.junit.Test;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 public class AbuseFilterTest {
+    AbuseFilter filter = new AbuseFilter();
 
     @Test
     public void whenAddInputStreamOfStringAndFilterAbuseThanFiltered() {
         String[] abuse = {"TEST", "bad"};
-        AbuseFilter filter = new AbuseFilter();
-        String input = "TEST bad string for testing bad";
+        String input = "TEST    bad string for testing bad\n"
+                + "Second bad string";
         InputStream in = new ByteArrayInputStream(input.getBytes());
         OutputStream out = new ByteArrayOutputStream();
         filter.dropAbuses(in, out, abuse);
-        String expected = "string for testing "; //в конце всегда прилипает пробел
+        String expected = "string for testing\n"
+                + "Second string\n";
         assertThat(out.toString(), is(expected));
     }
 
+    @Test
+    public void whenAddInputStreamOfStringAndFilterAbuseThanFilteredSecondMethod() throws IOException {
+        String[] abuse = {"TEST", "bad"};
+        String input = "TEST    bad string for testing bad\n"
+                + "Second bad string";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        OutputStream out = new ByteArrayOutputStream();
+        filter.dropAbusesSecond(in, out, abuse);
+        String expected = "string for testing\n"
+                + "Second string";
+        assertThat(out.toString(), is(expected));
+    }
+
+    @Test
+    public void whenAddInputStreamOfStringAndFilterAbuseThanFilterUniversalMethod() throws IOException {
+        String[] abuse = {"TEST", "bad"};
+        String input = "TEST    bad string for testing bad\n"
+                + "Second bad string";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        OutputStream out = new ByteArrayOutputStream();
+        filter.dropAbuseUniversal(in, out, abuse);
+        String expected = "string for testing\n"
+                + "Second string\n";
+        assertThat(out.toString(), is(expected));
+    }
+
+    @Test
+    public void whenFilterFileByUniversalMethodThanTrue() throws IOException {
+        String[] abuse = {"test", "bad"};
+        List<String> source = List.of(
+                "filter test file",
+                "filter will remove bad words",
+                "bad words will disappear"
+        );
+        File input = File.createTempFile("tmp", ".txt");
+        input.deleteOnExit();
+        File output = File.createTempFile("tmp", ".txt");
+        output.deleteOnExit();
+        try (FileWriter fw = new FileWriter(input)) {
+            for (String s : source) {
+                fw.write(s + "\n");
+            }
+        }
+        try (FileInputStream in = new FileInputStream(input);
+             FileOutputStream out = new FileOutputStream(output)
+        ) {
+            filter.dropAbuseUniversal(in, out, abuse);
+        }
+        List<String> result = new ArrayList<>();
+        try (FileReader fr = new FileReader(output);
+        BufferedReader br = new BufferedReader(fr)) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                result.add(line);
+            }
+        }
+        List<String> expected = List.of(
+                "filter file",
+                "filter will remove words",
+                "words will disappear"
+        );
+        assertThat(result, is(expected));
+    }
 }
