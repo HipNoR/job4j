458f97fd7127fbf311285b0b63654aa410086aff
HipNoR <hipnorosva@gmail.com>
2019-02-13 22:53:18 +0300

    converter int to BigDecimal

--- chapter_001/src/main/java/ru/job4j/converter/Converter.java
+++ chapter_001/src/main/java/ru/job4j/converter/Converter.java
@@ -1,5 +1,9 @@
 package ru.job4j.converter;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+
 /**
  * Class Converter - currency converter.
  * Convert RUB to USD, RUB to EUR, USD to RUB, EUR to RUB.
@@ -8,14 +12,17 @@ package ru.job4j.converter;
  * @version 0.1
  */
 public class Converter {
+    private final MathContext context = new MathContext(2, RoundingMode.HALF_EVEN);
+    private final BigDecimal eur = new BigDecimal(70.00, this.context);
+    private final BigDecimal usd = new BigDecimal(60.00, this.context);
 
     /**
      * Convert RUB to EUR.
      * @param value RUB.
      * @return EUR.
      */
-    public int rubleToEuro(int value) {
-        return value / 70;
+    public BigDecimal rubleToEuro(BigDecimal value) {
+        return value.divide(eur, this.context);
     }
 
     /**
@@ -23,8 +30,8 @@ public class Converter {
      * @param value RUB.
      * @return USD
      */
-    public int rubleToDollar(int value) {
-        return value / 60;
+    public BigDecimal rubleToDollar(BigDecimal value) {
+        return value.divide(usd, this.context);
     }
 
     /**
@@ -32,8 +39,8 @@ public class Converter {
      * @param value EUR.
      * @return RUB.
      */
-    public int euroToRuble(int value) {
-        return value * 70;
+    public BigDecimal euroToRuble(BigDecimal value) {
+        return value.multiply(eur, this.context);
     }
 
     /**
@@ -41,7 +48,6 @@ public class Converter {
      * @param value USD.
      * @return RUB
      */
-    public int dollarToRuble(int value) {
-        return value * 60;
-    }
+    public BigDecimal dollarToRuble(BigDecimal value) {
+        return value.multiply(usd, this.context);    }
 }
--- chapter_001/src/test/java/ru/job4j/converter/ConverterTest.java
+++ chapter_001/src/test/java/ru/job4j/converter/ConverterTest.java
@@ -2,35 +2,45 @@ package ru.job4j.converter;
 
 import org.junit.Test;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class ConverterTest {
+    private final static MathContext CONTEXT = new MathContext(2, RoundingMode.HALF_EVEN);
+
     @Test
     public void when60RubleToDollarThen1() {
         Converter converter = new Converter();
-        int result = converter.rubleToDollar(60);
-        assertThat(result, is(1));
+        BigDecimal result = converter.rubleToDollar(new BigDecimal(60.00, CONTEXT));
+        BigDecimal expected = new BigDecimal(1.00, CONTEXT);
+        assertThat(result, is(expected));
     }
 
     @Test
     public void when70RubleToEuroThen1() {
         Converter converter = new Converter();
-        int result = converter.rubleToEuro(70);
-        assertThat(result, is(1));
+        BigDecimal result = converter.rubleToEuro(new BigDecimal(70.00, CONTEXT));
+        BigDecimal expected = new BigDecimal(1.00, CONTEXT);
+        assertThat(result, is(expected));
     }
 
     @Test
     public void when1DollarToRubleThen60() {
         Converter converter = new Converter();
-        int result = converter.dollarToRuble(1);
-        assertThat(result, is(60));
+        BigDecimal result = converter.dollarToRuble(new BigDecimal(1.00, CONTEXT));
+        BigDecimal expected = new BigDecimal(60.00, CONTEXT);
+        assertThat(result, is(expected));
     }
 
     @Test
     public void when1EuroToRubleThen70() {
         Converter converter = new Converter();
-        int result = converter.euroToRuble(1);
-        assertThat(result, is(70));
+        BigDecimal result = converter.euroToRuble(new BigDecimal(1.00, CONTEXT));
+        BigDecimal expected = new BigDecimal(70.00, CONTEXT);
+        assertThat(result, is(expected));
     }
 }
